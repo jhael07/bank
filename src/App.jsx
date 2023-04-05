221951a5e7b473816452d10caffb1d3c49f77e5d
jhael <jhaelr007.com>
2023-04-05 15:40:17 -0400

    vamo a ver

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import Login from "./pages/Start/Login/Login";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "/react-router-dom";
 import Register from "./pages/Start/Register/Register";
 
 export default function App() {
     return (
         <BrowserRouter>
             <Routes>
-                <Route element={<Login />} path="/"></Route>
-                <Route element={<Register />} path="/register"></Route>
+                <Route element={<Login />} path="./"></Route>
+                <Route element={<Register />} path="./register"></Route>
             </Routes>
         </BrowserRouter>
     );
